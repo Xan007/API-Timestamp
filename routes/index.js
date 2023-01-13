ed87f6bbf1301b6e6626b4c1109deab31277ea5b
Sander Sierra <sierrasander29@gmail.com>
2023-01-13 14:17:14 -0500

    Añade endpoint a /api

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,21 @@ import { checkDateParam } from "../validators/dateValidator.js";
 
 const router = Router()
 
+const getJSONDate = (date) => {
+    return {
+        "unix": date.getTime(),
+        "utc": date.toUTCString()
+    }
+}
+
+router.get("/", async(req, res) => {
+    res.json(getJSONDate(new Date()))
+})
+
 router.get("/:date", checkDateParam, async(req, res) => {
     const { date } = req.params
 
-    res.json({
-        "unix": date.getTime(),
-        "utc": date.toUTCString()
-    })
+    res.json(getJSONDate(date))
 })
 
 export default router
